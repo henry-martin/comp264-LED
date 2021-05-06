f1c295b96764c5c9d46baaaf99341eb21bdc388f
kmadhok <72879711+kmadhok@users.noreply.github.com>
2021-05-06 17:36:11 -0500

    Update Voice_to_text_main.py

--- Voice_to_text_main.py
+++ Voice_to_text_main.py
@@ -17,15 +17,11 @@ while (1):
 
         with sr.Microphone() as source2:
 
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
+    
             r.adjust_for_ambient_noise(source2, duration=0.2)
 
-            # listens for the user's input
             audio2 = r.listen(source2)
 
-            # Using ggogle to recognize audio
             NoteText = r.recognize_google(audio2)
             NoteText = NoteText.lower()
 
@@ -35,9 +31,7 @@ while (1):
             print(NoteText)
             SpeakText(NoteText)
 
-            #sourceFile = open('demo.txt', 'w')
-            #print("Did you say " + MyText)
-            #sourceFile.close()
+
 
     except sr.RequestError as e:
         print("Could not return results; {0}".format(e))
